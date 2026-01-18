3453e45337384a6f87f798c79d62dd4ad266263e
cuongnguyen3108 <nguyencaocuong31082001@gmail.com>
2026-01-18 21:44:46 +0700

    update code LoginPageUI.java

--- src/test/java/tests/login/ui/LoginPageUI.java
+++ src/test/java/tests/login/ui/LoginPageUI.java
@@ -7,6 +7,6 @@ public class LoginPageUI {
     public static final By PASSWORD_FIELD = By.id("password");
     public static final By LOGIN_BUTTON   = By.id("login-button");
     public static final By ERROR_MESSAGE  = By.cssSelector("[data-test='error']");
-    public static final By INVENTORY_ITEM = By.className("inventory_item");
+    public static final By INVENTORY_ITEM = By.xpath("//div[@class='inventory_item']");
     public static final By INVENTORY_PRODUCT_SORT =  By.className("product_sort_container");
 }
